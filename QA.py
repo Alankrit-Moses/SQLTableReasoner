b22cb324a39a3fc0648c61c0326d5978a7f67835
Alankrit-Moses <113947973+Alankrit-Moses@users.noreply.github.com>
2025-05-15 18:03:39 -0500

    QA

--- QA.py
+++ QA.py
@@ -12,8 +12,8 @@ class QA:
         for name in names:
             prompt+=self.dbconn.get_table_example(name)+'\n\n'
         prompt+='Narrow down the tables from the aforementioned list of tables can help answer the following question.'+'\n'
-        prompt+='Question: '+question+'\n\n'
-        prompt+='Give your output in a json format as such:'
+        prompt+='Question: '+question
+        prompt+='\nGive your output in a json format as such:\n'
         prompt+='{"selected_tables":["name of first relevant table", "name of second relevant table",...,"name of last relevant table"}'
         prompt+='\nRemember that your output should only contain json and no leading or trailing text, so that I can directly parse the output as a json'
         print(prompt)
