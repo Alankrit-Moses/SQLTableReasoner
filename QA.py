ae7e7cd47b67b0afe4163a8f307c6100ff2c971a
Alankrit-Moses <113947973+Alankrit-Moses@users.noreply.github.com>
2025-05-15 18:01:01 -0500

    QA

--- QA.py
+++ QA.py
@@ -14,7 +14,7 @@ class QA:
         prompt+='Narrow down the tables from the aforementioned list of tables can help answer the following question.'+'\n'
         prompt+='Question: '+question+'\n\n'
         prompt+='Give your output in a json format as such:'
-        prompt+='\{"selected_tables":["name of first relevant table", "name of second relevant table",...,"name of last relevant table"\}'
+        prompt+='{"selected_tables":["name of first relevant table", "name of second relevant table",...,"name of last relevant table"}'
         prompt+='\nRemember that your output should only contain json and no leading or trailing text, so that I can directly parse the output as a json'
         print(prompt)
         # self.ollama.query(prompt)
