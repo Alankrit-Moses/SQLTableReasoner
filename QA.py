2793660a1ac51bd49c58231b88dac8ea7a9aab61
Alankrit-Moses <113947973+Alankrit-Moses@users.noreply.github.com>
2025-05-15 19:34:33 -0500

    QA

--- QA.py
+++ QA.py
@@ -8,14 +8,14 @@ class QA:
         self.ollama = Ollama()
         print(self.dbconn.get_table_example('marine_landings'))
     
-    def query(self, question, table_name):
+    def sql_extractor(self, question, table_name):
         prompt = '\n'.join(EXAMPLES)
         prompt+= '---END OF EXAMPLES---\n'
         prompt+= 'Actual table to be considered for SQL query:\n\nTable Name to be used in SQL query: '+table_name+'\n'
         prompt+= self.dbconn.get_table_example(table_name)
         prompt+='\nQuestion: '+question
-        prompt+='\n\nIMPORTANT: The output should only contain the SQL query, such that it can directly be executed.'
-        print(self.ollama.query(prompt))
+        prompt+='\n\nIMPORTANT: The output should only contain the SQL query, such that it can directly be executed. Do not produce the think tokens or the <think> block.'
+        answer = self.ollama.query(prompt)
 
 
 qa = QA()
